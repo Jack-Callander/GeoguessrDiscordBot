c409d784a2a7298fe89397e80ded99985b4d9a35
JackCallander <z5207123@cse.unsw.edu.au>
2021-09-14 18:59:26 +1000

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+// Bot Code Here
+
+
+
+
+
+
+
+client.once('ready', () => { console.log("Geoguessr Bot Online"); });
+client.login('ODg3MjU1MTU3ODk5OTIzNTA2.YUBewg.DkE7qbBubxYsdbZmglaSkNIgi4I');

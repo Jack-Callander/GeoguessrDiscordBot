7887cf9e64a6d0bb3f8e73470daa3ba427dbfbdc
mdio49 <marco9.dio@gmail.com>
2021-09-15 10:29:59 +1000

    Added distances and details for each round. Also cleaned up the code a little bit.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 from src.ChromeDevice import ChromeDevice
-from src.GeoguessrResult import GeoguessrResult, Time
+from src.GeoguessrResult import Distance, GeoguessrResult, Rules, Time, Units
 from src.JSDevice import JSDevice

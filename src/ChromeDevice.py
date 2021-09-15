6253836274023f1176250ae35ce8039596bed8d8
JackCallander <z5207123@cse.unsw.edu.au>
2021-09-15 17:15:16 +1000

    Implemented compatibility with replit

--- src/ChromeDevice.py
+++ src/ChromeDevice.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from src.JSDevice import JSDevice
+import os.path
 
 class ChromeDevice(JSDevice):
 
@@ -13,8 +14,15 @@ class ChromeDevice(JSDevice):
             return self.cache[url]
         
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
-        driver = webdriver.Chrome(options=chrome_options)
+        chrome_options.add_argument('--headless')
+        driver = None
+
+        if (os.path.isfile(".usingreplit")):
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-gpu')
+            driver = webdriver.Chrome(options=chrome_options)
+        else:
+            driver = webdriver.Chrome("D:/chromedriver.exe", options=chrome_options)
         driver.get(url)
         html = driver.page_source
         driver.quit()
